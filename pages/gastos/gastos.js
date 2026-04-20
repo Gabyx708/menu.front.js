bebd420dcd91e577d20a186ba0c072ce271c9af0
Gabyx708 <matiascoria427@gmail.com>
2026-04-20 13:59:43 -0300

    se agrego un selector de mes para los gastos

--- pages/gastos/gastos.js
+++ pages/gastos/gastos.js
@@ -1,36 +1,46 @@
 import getGastosMes from "../../services/web/getGastosMes.js"
 
-window.onload = async () =>
-{
-    let response = await getGastosMes();
+window.onload = async () => {
 
+    const selectMes = document.getElementById("mes");
+
+    // Mes actual automático
+    const mesActual = new Date().getMonth() + 1;
+    selectMes.value = mesActual;
+
+    await cargarDatos(mesActual);
+
+    // Evento cuando cambia el mes
+    selectMes.addEventListener("change", async () => {
+        const mesSeleccionado = selectMes.value;
+        await cargarDatos(mesSeleccionado);
+    });
+};
+
+
+async function cargarDatos(mes) {
+    
+    let response = await getGastosMes({ mes });
     let responseJson = await response.json();
-
     let dataGastos = responseJson.data;
 
     PintarDatosGastos(dataGastos);
 }
 
 
-
-function PintarDatosGastos(dataGastos)
-{  
+function PintarDatosGastos(dataGastos) {  
     let montoTotal = dataGastos.montos.montoTotal;
     let montoDescontar = dataGastos.montos.montoApagar;
 
     let pedidosCompletados = dataGastos.montos.cantidadPedidosConfirmados;
     let pedidosCancelados = dataGastos.montos.cantidadPedidosCancelados;
-    console.log(dataGastos);
 
-
-    let montoTotalStr = "$"+new Intl.NumberFormat("es-AR").format(montoTotal);
-    let montoDescontarStr = "$"+new Intl.NumberFormat("es-AR").format(montoDescontar);
-
+    let montoTotalStr = "$" + new Intl.NumberFormat("es-AR").format(montoTotal);
+    let montoDescontarStr = "$" + new Intl.NumberFormat("es-AR").format(montoDescontar);
 
     document.getElementById("monto-total").textContent = montoTotalStr;
     document.getElementById("monto-descontar").textContent = montoDescontarStr;
 
     document.getElementById("cant-pedidos-completados").textContent = pedidosCompletados;
     document.getElementById("cant-pedidos-cancelados").textContent = pedidosCancelados;
-
 }
--- services/web/getGastosMes.js
+++ services/web/getGastosMes.js
@@ -1,11 +1,13 @@
 import API_URL from "../../config/ApiConfig.js";
 import { getDni, getToken } from "../local/guardarUsuario.js";
 
-export default async function getGastosMes() 
+export default async function getGastosMes({mes}) 
 {
+  console.log("ejecuto")
+
     const dni = getDni();
     const jwt = getToken();
-    const endpoint = `${API_URL}/usuario/${dni}/gastos`;
+    const endpoint = `${API_URL}/usuario/${dni}/gastos?month=${mes}`;
 
     const request = {
         method: "GET",
