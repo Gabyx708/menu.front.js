504bf99c1e0a4e5cc2f6a669884c343b656884ad
Gabyx708 <matiascoria427@gmail.com>
2026-05-04 15:21:16 -0300

    cambio estetico en las tarjetas de menues

--- ui/components/tarjetaOpcionMenu/tarjetaOpcionMenuComponent.js
+++ ui/components/tarjetaOpcionMenu/tarjetaOpcionMenuComponent.js
@@ -2,22 +2,24 @@ export default function TarjetaOpcionMenuComponent(opcion) {
     const formatter = new Intl.NumberFormat('es-AR', {
         style: 'currency',
         currency: 'ARS',
-        minimumFractionDigits: 2,
+        minimumFractionDigits: 0,
     });
 
     const precioFormateado = formatter.format(opcion.precio);
-    const stockRestante = opcion.stock;
     const sinStock = opcion.stock === 0 ? ' sin-stock' : '';
-
-    let descripcion = opcion.descripcion; 
 
     return `
         <div class="tarjeta-opcion${sinStock}">
-            <h3><strong>${new String(descripcion).toUpperCase()}</strong> </h3>
-            <p><strong>Precio:</strong> ${precioFormateado}</p>
-            <p><strong>Quedan:</strong> ${stockRestante}</p>
-            <p><strong>Pedidos:</strong> ${opcion.solicitados}</p>
-            <button class="btn-pedir-opcion" data-id="${opcion.idPlato}">PEDIR</button>
+            <h3>${opcion.descripcion.toUpperCase()}</h3>
+            
+            <p>${precioFormateado}</p>
+            
+            <p>Stock: ${opcion.stock}</p>
+            <p>Pedidos: ${opcion.solicitados}</p>
+
+            <button class="btn-pedir-opcion" data-id="${opcion.idPlato}">
+                PEDIR
+            </button>
         </div>
     `;
 }
